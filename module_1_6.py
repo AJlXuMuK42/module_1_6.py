4d8cddb673edb811fc03dce1f80c6a0a0591de3a
AJlXuMuK42 <ajlxumuk65@gmail.com>
2024-12-25 21:19:11 +0700

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,19 @@
+my_dict={'Ilia':1994,'Mike':1990,'Jake':2000,'Zik':2007}
+print(my_dict)
+print(my_dict['Ilia'])
+print(my_dict.get('Simon', 'not found'))
+my_dict.update({'Mike':1999, 'Salvador':2025})
+print(my_dict)
+Z=my_dict.pop('Zik')
+print(Z)
+print(my_dict)
+
+
+
+my_set={1,1,1,2,2,2,3,3,'Prog','Prog', False, False}
+print(my_set)
+my_set.add(34),my_set.add('Lion')
+my_set.discard(1)
+print(my_set)
+
+
